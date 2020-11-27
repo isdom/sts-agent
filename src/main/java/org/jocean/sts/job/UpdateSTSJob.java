573a24e17d28ee6fb3ab87d55dc8058b2e129519
ming.ma <isdom@qq.com>
2020-11-27 17:30:45 +0800

    获取 ecs.id 信息

--- src/main/java/org/jocean/sts/job/UpdateSTSJob.java
+++ src/main/java/org/jocean/sts/job/UpdateSTSJob.java
@@ -6,16 +6,20 @@ import javax.inject.Named;
 import org.apache.curator.framework.CuratorFramework;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 public class UpdateSTSJob {
 
     private static final Logger LOG = LoggerFactory.getLogger(UpdateSTSJob.class);
 
     void update() {
-        LOG.info("update: zkconn info {}", _curator);
+        LOG.info("update: zkconn info {}, ecs instance {}", _curator, _instanceId);
     }
 
     @Inject
     @Named("${zkconn.name}")
     CuratorFramework _curator;
+
+    @Value("${ecs.id}")
+    String _instanceId;
 }
