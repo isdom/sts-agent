1dd1bb69541363f83cdad0dd267e71b13633783e
ming.ma <isdom@qq.com>
2020-11-28 19:28:57 +0800

    make UpdateSTSJob prototype

--- src/main/java/org/jocean/sts/job/UpdateSTSJob.java
+++ src/main/java/org/jocean/sts/job/UpdateSTSJob.java
@@ -7,7 +7,9 @@ import org.apache.curator.framework.CuratorFramework;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 
+@Scope("prototype")
 public class UpdateSTSJob {
 
     private static final Logger LOG = LoggerFactory.getLogger(UpdateSTSJob.class);
