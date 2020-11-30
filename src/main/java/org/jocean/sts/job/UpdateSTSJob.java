5d9ebf92302ff3e3c758cdaffd471e359ab82b1b
ming.ma <isdom@qq.com>
2020-11-30 17:35:34 +0800

    使用 RpcDelegater 创建 MetadataAPI.STSTokenBuilder 实例

--- src/main/java/org/jocean/sts/job/UpdateSTSJob.java
+++ src/main/java/org/jocean/sts/job/UpdateSTSJob.java
@@ -6,7 +6,7 @@ import javax.inject.Named;
 import org.apache.curator.framework.CuratorFramework;
 import org.jocean.aliyun.ecs.MetadataAPI;
 import org.jocean.http.RpcExecutor;
-import org.jocean.svr.annotation.RpcFacade;
+import org.jocean.rpc.RpcDelegater;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,13 +19,12 @@ public class UpdateSTSJob {
 
     void update() {
         LOG.info("update by {}: executor {}, zkconn info {}, ecs instance {}", this, _executor, _curator, _instanceId);
-//        getststoken.roleName(_ecsRole).call().subscribe(resp ->
-//            LOG.info("ak_id {}/ak_secret {}/token {}",
-//                    resp.getAccessKeyId(), resp.getAccessKeySecret(), resp.getSecurityToken()));
+        final MetadataAPI.STSTokenBuilder getststoken =
+                RpcDelegater.rpc(MetadataAPI.STSTokenBuilder.class).invoker(inter2any -> _executor.submit(inter2any)).build();
+        getststoken.roleName(_ecsRole).call().subscribe(resp ->
+            LOG.info("ak_id {}/ak_secret {}/token {}",
+                    resp.getAccessKeyId(), resp.getAccessKeySecret(), resp.getSecurityToken()));
     }
-
-    @RpcFacade
-    MetadataAPI.STSTokenBuilder  getststoken;
 
     @Inject
     RpcExecutor _executor;
